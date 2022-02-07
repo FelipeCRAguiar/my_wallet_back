0801aaf3e4a822c312739da2726a65ec4d9881ba
Felipe <mr.tvz569@gmail.com>
2022-02-07 15:50:33 -0300

    added auth route

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -3,7 +3,7 @@ import userSchema from "../schemas/useSchema.js"
 export default function validateUser(req, res, next) {
     const validation = userSchema.validate(req.body)
 
-    if (validation.error){
+    if (validation.error) {
         res.sendStatus(422)
         return
     }
