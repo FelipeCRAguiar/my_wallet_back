d5c9c76827b0cd5757945683416f3359e550a522
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-02-15 11:13:21 -0300

    Preparando revisão de código

--- schemas/newUserSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi"
-
-const newUserSchema = joi.object({
-    username: joi.string().required(),
-    email: joi.string().required().email(),
-    password: joi.string().required()
-})
-
-export default newUserSchema
